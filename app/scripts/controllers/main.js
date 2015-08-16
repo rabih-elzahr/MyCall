955ab387dbfe6885977427d0996528726f3e4714
Administrator <rabih.elzahr@icloud.com>
2015-08-16 16:54:02 -0400

    Example with Firebase 3-way data binding

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,29 @@
  * # MainCtrl
  * Controller of the myCallApp
  */
-angular.module('myCallApp')
-  .controller('MainCtrl', function ($scope) {
+angular.module('myCallApp', ['firebase'])
+  .controller('MainCtrl', function ($scope, $firebaseObject, $firebaseArray) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    var ref = new Firebase("https://mycall.firebaseio.com/");
+
+    $scope.submitData = {};
+    $scope.submitData.username = '';
+    // $scope.data = $firebaseObject(ref);
+    $scope.data = $firebaseArray(ref);
+debugger;
+	// syncObject.$bindTo($scope, "data");
+
+    $scope.postData = function () {
+    	debugger;
+	 	$scope.data.$add({
+	      text: $scope.submitData.username
+    	});
+
+    	$scope.submitData.username = '';
+    };
   });
